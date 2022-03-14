c93c35334e0101439ad163258986e133c2eb31a5
Srikanth Vidyasagar Sadhu <srikanthvidyasagar.sadhu@sri.com>
2022-03-14 14:50:42 -0700

    updated multigpu test

--- scripts/ddp_multigpu_test.py
+++ scripts/ddp_multigpu_test.py
@@ -1,20 +1,28 @@
 import torch
 import os
 import torch.distributed as dist
+import torch.multiprocessing as mp
+
+def test(indx):
+    dist.init_process_group(backend="nccl",
+                            init_method="env://",
+                            world_size=3,
+                            rank=indx)
+    tensor_list = []
+    # for dev_idx in range(torch.cuda.device_count()):
+    # for dev_idx in range(1):
+    tensor_list.append(torch.FloatTensor([indx]).cuda(indx))
+
+    output_list = [torch.FloatTensor([0]).cuda(indx) for _ in range(3)]
+
+    dist.all_gather_multigpu(output_list, tensor_list)
+
+    print('done')
 
 # os.environ['MASTER_ADDR'] = '130.107.72.224' #cuda0003
 os.environ['MASTER_ADDR'] = '130.107.72.207'  # tulsi
 # os.environ['MASTER_ADDR'] = '130.107.72.220'  # cuda0001
 os.environ['MASTER_PORT'] = '8888'
 os.environ['TORCH_DISTRIBUTED_DEBUG'] = 'DETAIL'
-dist.init_process_group(backend="nccl",
-                        init_method="env://",
-                        world_size=3,
-                        rank=0)
-tensor_list = []
-for dev_idx in range(torch.cuda.device_count()):
-    tensor_list.append(torch.FloatTensor([1]).cuda(dev_idx))
-
-dist.all_reduce_multigpu(tensor_list)
-
-print('done')
+mp.spawn(test, nprocs=2)
+# test(2) # on cuda0003
